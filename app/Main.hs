6d9fdfcf81298473efba680ce260ca5bb89979e9
mac10688 <mac10688@protonmail.com>
2019-05-28 14:47:02 -0700

    Writing my own merge sort that filters out duplicates

--- app/Main.hs
+++ app/Main.hs
@@ -5,21 +5,22 @@ import System.Environment
 import Data.Foldable
 import Data.List
 import Data.Ord
+import Merge
 
 main :: IO ()
 main = do
   args <- getArgs
   let parsedItems = parseInput args
   putStrLn "Dates:"
-  let dates = sort $ filter (\case ParsedDate _ -> True; otherwise -> False) $ parsedItems
+  let dates = msort $ filter (\case ParsedDate _ -> True; otherwise -> False) $ parsedItems
   forM_ dates print
   putStrLn "\n\nPhoneNumbers:"
-  let phoneNumbers = sort $ filter (\case ParsedPhoneNumber _ -> True; otherwise -> False) $ parsedItems
+  let phoneNumbers = msort $ filter (\case ParsedPhoneNumber _ -> True; otherwise -> False) $ parsedItems
   forM_ phoneNumbers print
   putStrLn "\n\nWords:"
-  let words = sort $ filter (\case ParsedWord _ -> True; otherwise -> False) $ parsedItems
+  let words = msort $ filter (\case ParsedWord _ -> True; otherwise -> False) $ parsedItems
   forM_ words print
   putStrLn "\n\nNumbers"
-  let numbers = sort $ filter (\case ParsedNumber _ -> True; otherwise -> False) $ parsedItems
+  let numbers = msort $ filter (\case ParsedNumber _ -> True; otherwise -> False) $ parsedItems
   forM_ numbers print
 
---
+++ src/Merge.hs
@@ -0,0 +1,22 @@
+module Merge
+  (
+    msort 
+  ) where
+
+merge :: Ord a => [a] -> [a] -> [a]
+merge [] ys = ys
+merge xs [] = xs
+merge (x:xs) (y:ys) | x < y = x : merge xs (y:ys)
+                    | x > y = y : merge (x:xs) ys
+                    | otherwise = merge xs (y:ys)
+
+
+halve :: [a] -> ([a],[a])
+halve xs = (take lhx xs, drop lhx xs)
+  where lhx = length xs `div` 2
+
+msort :: Ord a => [a] -> [a]
+msort [] = []
+msort [x] = [x]
+msort xs = merge (msort left) (msort right)
+  where (left, right) = halve xs
